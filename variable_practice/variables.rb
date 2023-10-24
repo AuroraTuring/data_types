5a586e2e8f34ceacf64f679ef6f26ee2c4c01c47
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-24 13:39:52 -0600

    Complete assignment

--- variable_practice/variables.rb
+++ variable_practice/variables.rb
@@ -1,3 +1,23 @@
-name = "Aurora"
+name = "Nicole"
 favorite_color = "Purple" 
 
+age = 31 
+favorite_number = 8 
+
+bank_balance = 138.75
+gpa = 3.2 
+
+is_colorful = true 
+of_age = true 
+
+
+name = "Aurora" 
+age = 31 
+gpa = 3.5 
+is_colorful = false 
+
+
+puts name 
+puts age - favorite_number
+puts "I have #{bank_balance} in my account."
+puts is_colorful 
